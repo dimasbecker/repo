895fbbd13621e17a33366e0eba0db0243f84719f
Dimas <dimasbecker@outlook.com>
2019-10-27 20:38:12 -0300

    Original

---
+++ Parcial_2 v1.4.0/Parcial_2/Jugador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial_2
+{
+    public class Jugador
+    {
+        int max_vida;
+        int vida;
+        int daño;
+
+        public Jugador(Bitmap btmp)
+        {
+            
+        }
+    }
+}
